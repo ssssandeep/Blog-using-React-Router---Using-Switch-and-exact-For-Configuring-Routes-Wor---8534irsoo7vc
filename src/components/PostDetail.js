3f2569b81df366742ac1552b2d38f68458d02cf3
ssssandeep <ssssandeep57@gmail.com>
2023-02-07 18:07:45 +0000

    updated

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -4,9 +4,9 @@ import { useParams } from 'react-router-dom';
 export default function PostDetail({ match }) {
   const { id } = useParams();
   return (
-    <div>
+    <h1>
       Post Details : {id}
-    </div>
+    </h1>
   )
 }
   
